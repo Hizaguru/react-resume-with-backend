156715dd069b2cbb7986e9c95b35ff6a4ed92951
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2022-10-01 15:16:20 +0300

    update dependencies2

--- react-resume-template/src/data/data.tsx
+++ react-resume-template/src/data/data.tsx
@@ -73,14 +73,8 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
-      </p>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        I'm a Finland based <strong className="text-stone-100">Full Stack Software Engineer, audio </strong>and
+        <strong className="text-stone-100"> graphic designer</strong> creating all kinds of projects that come to mind.
       </p>
     </>
   ),
