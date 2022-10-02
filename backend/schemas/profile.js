93a95949f53f35169a6a6b2073349e76fac7a4be
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2022-10-02 17:58:05 +0300

    Add new schemas

---
+++ backend/schemas/profile.js
@@ -0,0 +1,17 @@
+export default {
+  name: "profile",
+  title: "Profile",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+    },
+  ],
+};
