e63e2568254ca79851b3f0c77be16056d4a271c9
juck <juck@DESKTOP-CF449NH.localdomain>
2022-10-05 20:26:26 +0300

    React env file works

--- react-resume-template/src/client.js
+++ react-resume-template/src/client.js
@@ -1,12 +1,13 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+
 export const client = sanityClient({
-  projectId: 'jjd5fsef',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 const builder = imageUrlBuilder(client);
 
