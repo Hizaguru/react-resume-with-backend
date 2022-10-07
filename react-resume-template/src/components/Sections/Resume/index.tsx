6114df3c39cbf52a264fd6892d33d5f76d29d6c5
juck <juck@DESKTOP-CF449NH.localdomain>
2022-10-07 18:45:04 +0300

    skill-section done

--- react-resume-template/src/components/Sections/Resume/index.tsx
+++ react-resume-template/src/components/Sections/Resume/index.tsx
@@ -1,23 +1,53 @@
 import { FC, memo } from 'react';
 
-import { SectionId, skills } from '../../../data/data';
+import 'react-multi-carousel/lib/styles.css';
+import { SectionId } from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
-import { SkillGroup } from './Skills';
+
+
 
 const Resume: FC = memo(() => {
+  const skills = [
+    {
+      // eslint-disable-next-line
+      skillname: "Coding (React, Typescript, Python)",
+    },
+    {
+      skillname: "Game Development (Unity, Unreal Engine)",
+    },
+    {
+      skillname: "Operating Systems (Ubuntu, Kali, Windows)",
+    },
+    {
+      skillname: "Audio Designing & Mixing (Ableton live 10)",
+    },
+    {
+      skillname: "Graphic Designing (Blender)",
+    },
+  ]
+
+
+
   return (
     <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
       <div className="flex flex-col divide-y-2 divide-neutral-300">
         <ResumeSection title="Skills">
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {skills.map((skillgroup, index) => (
-              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
-            ))}
-          </div>
+          <ul className='lg:text-2xl sm:text-xl text-center lg:text-left'>
+            {skills &&
+              skills.map((item) => {
+                return (
+                  <li key={item.skillname} className='p-3'>
+                    <span className={item.skillname.toLowerCase()} />
+                    <em>{item.skillname}</em>
+                    <hr className=' md:w-1/4 lg:w-3/5 mt-6' />
+                  </li>
+                );
+              })}
+          </ul>
         </ResumeSection>
-      </div>
-    </Section>
+      </div >
+    </Section >
   );
 });
 
