222d072aa0841b3e5402a17b05868ecd669c6665
juck <juck@DESKTOP-CF449NH>
2022-10-08 20:12:17 +0300

    Fix skills section

--- react-resume-template/src/components/Sections/Resume/index.tsx
+++ react-resume-template/src/components/Sections/Resume/index.tsx
@@ -12,14 +12,14 @@ const Resume: FC = memo(() => {
     <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
       <div className="flex flex-col divide-y-2 divide-neutral-300">
         <ResumeSection title="Skills">
-          <ul className='lg:text-2xl sm:text-xl text-center lg:text-left'>
+          <ul className='lg:text-2xl sm:text-xl md:text-left text-center lg:text-left'>
             {skills &&
               skills.map((item) => {
                 return (
                   <li key={item.skillname} className='p-3'>
                     <span className={item.skillname.toLowerCase()} />
                     <em>{item.skillname}</em>
-                    <hr className=' md:w-1/4 lg:w-3/5 mt-6' />
+                    <hr className=' md:w-1/2 lg:w-3/5 mt-6' />
                   </li>
                 );
               })}
