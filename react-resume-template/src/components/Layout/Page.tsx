6f4cebe761bc1c79e5fbf8d3acd29c534e87b1c3
juck <juck@DESKTOP-CF449NH>
2022-10-08 13:58:44 +0300

    Add tab icon

--- react-resume-template/src/components/Layout/Page.tsx
+++ react-resume-template/src/components/Layout/Page.tsx
@@ -1,12 +1,12 @@
-import {NextPage} from 'next';
+import { NextPage } from 'next';
 import Head from 'next/head';
-import {useRouter} from 'next/router';
-import {memo} from 'react';
+import { useRouter } from 'next/router';
+import { memo } from 'react';
 
-import {HomepageMeta} from '../../data/dataDef';
+import { HomepageMeta } from '../../data/dataDef';
 
-const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
-  const {asPath: pathname} = useRouter();
+const Page: NextPage<HomepageMeta> = memo(({ children, title, description }) => {
+  const { asPath: pathname } = useRouter();
 
   return (
     <>
@@ -16,11 +16,8 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
-
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link rel="shortcut icon" href="/images/tabs.png" />
+        <link href="/site.webmanifest" rel="manifest" key={"siteManifest"} />
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
