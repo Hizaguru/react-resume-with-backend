b871c934718a6e0f272a9e528f7bdc67b61fdcc2
juck <juck@DESKTOP-CF449NH>
2022-10-08 14:31:18 +0300

    add portfolio items

--- react-resume-template/src/data/data.tsx
+++ react-resume-template/src/data/data.tsx
@@ -7,17 +7,12 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+import porfolioImage1 from '../images/portfolio/portfolio-1.webp';
+import porfolioImage2 from '../images/portfolio/portfolio-2.webp';
+import porfolioImage3 from '../images/portfolio/portfolio-3.webp';
+import porfolioImage4 from '../images/portfolio/portfolio-4.webp';
+import porfolioImage5 from '../images/portfolio/portfolio-5.webp';
+import porfolioImage6 from '../images/portfolio/portfolio-6.webp';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -122,8 +117,8 @@ export const skills: Skills[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
+    title: 'Music (Ableton Live 10)',
+    description: 'I have a burning passion for music and audio worlds. My latest act in the music scene was Endocrine which released two singles.The latest, A.O.E was published in December 2020 and Red Glow year before that.',
     url: 'https://timbaker.me',
     image: porfolioImage1,
   },
@@ -134,59 +129,29 @@ export const portfolioItems: PortfolioItem[] = [
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
+    title: 'Music (Ableton Live 10)',
+    description: 'I have a burning passion for music and audio worlds. My latest act in the music scene was Endocrine which released two singles.The latest, A.O.E was published in December 2020 and Red Glow year before that.',
     url: 'https://timbaker.me',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
+    title: 'Project title 2',
     description: 'Give a short description of your project here.',
     url: 'https://timbaker.me',
     image: porfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
+    title: 'Music (Ableton Live 10)',
+    description: 'I have a burning passion for music and audio worlds. My latest act in the music scene was Endocrine which released two singles.The latest, A.O.E was published in December 2020 and Red Glow year before that.',
     url: 'https://timbaker.me',
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
+    title: 'Project title 2',
     description: 'Give a short description of your project here.',
     url: 'https://timbaker.me',
     image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage11,
-  },
+  }
 ];
 
 /**
