21a31f256863299b378cda8e89165b4ebf7e482f
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2022-10-09 09:47:39 +0300

    Fix the items

--- react-resume-template/src/components/Sections/Portfolio.tsx
+++ react-resume-template/src/components/Sections/Portfolio.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from '@heroicons/react/outline';
 import classNames from 'classnames';
 import Image from 'next/image';
 import { FC, memo, MouseEvent, useCallback, useEffect, useRef, useState } from 'react';
@@ -13,7 +14,7 @@ const Portfolio: FC = memo(() => {
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
       <div className="flex flex-col gap-y-8">
         <h2 className="self-center text-xl font-bold text-white">Check out some of my work</h2>
-        <div className="w-full columns-1 text-2xl md:columns-2 lg:columns-3">
+        <div className="w-full columns-1 md:columns-3 lg:columns-3">
           {portfolioItems.map((item, index) => {
             const { title, image } = item;
             return (
@@ -37,7 +38,7 @@ const Portfolio: FC = memo(() => {
 Portfolio.displayName = 'Portfolio';
 export default Portfolio;
 
-const ItemOverlay: FC<{ item: PortfolioItem }> = memo(({ item: { url, title, description, gitUrl, hasGitUrl } }) => {
+const ItemOverlay: FC<{ item: PortfolioItem }> = memo(({ item: { url, title, description } }) => {
   const [mobile, setMobile] = useState(false);
   const [showOverlay, setShowOverlay] = useState(false);
   const linkRef = useRef<HTMLAnchorElement>(null);
@@ -61,83 +62,23 @@ const ItemOverlay: FC<{ item: PortfolioItem }> = memo(({ item: { url, title, des
   );
 
   return (
-    <div
+    <a
       className={classNames(
         'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
         { 'opacity-0 hover:opacity-80': !mobile },
         showOverlay ? 'opacity-80' : 'opacity-0',
       )}
-    >
+      href={url}
+      onClick={handleItemClick}
+      ref={linkRef}
+      target="_blank">
       <div className="relative h-full w-full p-4">
         <div className="flex h-full w-full flex-col gap-y-2 overflow-y-scroll">
-          <h2 className="text-center font-bold text-white opacity-100">{title}</h2>
-          <p className="text-xl text-white opacity-100 sm:text-sm">{description}</p>
+          <h2 className="sm:text-4xl lg:text-xs text-center font-bold text-white opacity-100">{title}</h2>
+          <p className="sm:text-4xl lg:text-xs text-white opacity-100 sm:text-sm">{description}</p>
         </div>
-
-          {gitUrl ? (
-            <div>
-             <div className="absolute bottom-2 left-10 h-24 w-4 shrink-0 text-white sm:bottom-2 sm:right-2">
-             <a
-               className={classNames(
-                 'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
-                 { 'opacity-80 hover:opacity-60': !mobile },
-                 showOverlay ? 'opacity-80' : 'opacity-0',
-               )}
-               href={gitUrl}
-               onClick={handleItemClick}
-               ref={linkRef}
-               target="_blank">
-               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
-                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-               </svg>
-               <p className='text-center'>Code</p>
-             </a>
-           </div>
-
-           
-
-          <div className="absolute bottom-2 right-8 h-24 w-24 shrink-0 text-white sm:bottom-2 sm:right-2">
-          <a
-            className={classNames(
-              'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
-              { 'opacity-80 hover:opacity-60': !mobile },
-              showOverlay ? 'opacity-80' : 'opacity-0',
-            )}
-            href={url}
-            onClick={handleItemClick}
-            ref={linkRef}
-            target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-code-square" viewBox="0 0 16 16">
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-              <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
-            </svg>
-            <p >Project</p>
-          </a>
-        </div>
-        </div>
-          ) : (
-   
-            <div className="absolute bottom-2 right-8 h-24 w-24 shrink-0 text-white sm:bottom-2 sm:right-2">
-            <a
-              className={classNames(
-                'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
-                { 'opacity-80 hover:opacity-60': !mobile },
-                showOverlay ? 'opacity-80' : 'opacity-0',
-              )}
-              href={url}
-              onClick={handleItemClick}
-              ref={linkRef}
-              target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-code-square" viewBox="0 0 16 16">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
-              </svg>
-              <p >Project</p>
-            </a>
-          </div>
-          )}
-    
+        <ExternalLinkIcon className="absolute bottom-1 right-1 h-4 w-4 shrink-0 text-white sm:bottom-2 sm:right-2" />
       </div>
-    </div >
+    </a>
   );
 });
