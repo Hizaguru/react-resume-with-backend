8f0cfa117c4b8dbe7a252443a7d342423c824b42
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2024-06-12 19:33:33 +0300

    Refactor resume section to skill section

--- react-resume-template/src/data/data.tsx
+++ react-resume-template/src/data/data.tsx
@@ -1,33 +1,17 @@
-import {
-  AcademicCapIcon,
-  CalendarIcon, MapIcon,
-  OfficeBuildingIcon
-} from '@heroicons/react/outline';
+import {AcademicCapIcon, CalendarIcon, MapIcon, OfficeBuildingIcon} from '@heroicons/react/outline';
 import getAge from '../functions/calculateMyAge';
 import GithubIcon from '../components/Icon/GithubIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 
-
-
 import testimonialImage from '../images/testimonial.webp';
 
-
-import {
-  About,
-  ContactSection,
-  ContactType,
-  Hero,
-  HomepageMeta,
-  Skills,
-  Social,
-  TestimonialSection
-} from './dataDef';
+import {About, ContactSection, ContactType, Hero, HomepageMeta, Skills, Social, TestimonialSection} from './dataDef';
 
 /**
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'Jukkis\' portfolio',
+  title: "Jukkis' portfolio",
   description: 'My Portfolio website that was build with React typescript.',
   author: 'Jukka - Pekka Lappalainen',
   keywords: 'Portfolio, typescript, reat, Jukka-Pekka Lappalainen',
@@ -46,14 +30,13 @@ export const SectionId = {
   Testimonials: 'testimonials',
 } as const;
 
-export type SectionId = typeof SectionId[keyof typeof SectionId];
-
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
 
 /**
  * Hero section
  */
 export const heroData: Hero = {
-  name: 'I\'m Jukka - Pekka Lappalainen.',
+  name: "I'm Jukka - Pekka Lappalainen.",
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
@@ -75,14 +58,13 @@ export const heroData: Hero = {
  * About section
  */
 export const aboutData: About = {
-
   description: `I am an enthusiastic and experienced full-stack developer with a passion for creating innovative solutions. With a solid educational background in ICT, I have cultivated a diverse skill set that spans programming languages, frameworks, and problem-solving techniques.\n
   I dedicate my spare time to honing my programming skills and working on various personal projects. Beyond programming, I bring a unique perspective to my work, thanks to my extensive experience as a sound engineer. This background has exposed me to both software and hardware aspects, fostering a disciplined approach to troubleshooting and problem-solving.\n\n Feel free to explore my portfolio and get in touch if you're interested in partnering on an exciting venture or if you have any inquiries. Let's create something extraordinary together!`,
   aboutItems: [
-    { label: 'Location', text: 'Helsinki', Icon: MapIcon },
-    { label: 'Age', text: getAge('05/25/1988'), Icon: CalendarIcon },
-    { label: 'Study', text: 'Applied University of Metropolia', Icon: AcademicCapIcon },
-    { label: 'Employment', text: 'Fullstack Developer', Icon: OfficeBuildingIcon },
+    {label: 'Location', text: 'Helsinki', Icon: MapIcon},
+    {label: 'Age', text: getAge('05/25/1988'), Icon: CalendarIcon},
+    {label: 'Study', text: 'University of Jyväskylä', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Fullstack Developer', Icon: OfficeBuildingIcon},
   ],
 };
 
@@ -92,7 +74,7 @@ export const aboutData: About = {
 export const skills: Skills[] = [
   {
     // eslint-disable-next-line
-    skillname: "Coding (React, Typescript, Python, Java)",
+    skillname: 'Coding (React, Typescript, Python, Java)',
   },
   {
     skillname: 'Game Development (Unity, Unreal Engine)',
@@ -108,11 +90,9 @@ export const skills: Skills[] = [
   },
 ];
 
-
 /**
  * Portfolio section
  */
-
 
 /**
  * Testimonial section
@@ -122,22 +102,19 @@ export const testimonial: TestimonialSection = {
   testimonials: [
     {
       name: 'Steve Jobs',
-      text: 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven\'t found it yet, keep looking. Don\'t settle. As with all matters of the heart, you\'ll know when you find it.',
-
+      text: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
     },
     {
       name: 'Emo Philips',
-      text: 'A computer once beat me at chess, but it was no match for me at kick boxing.'
-
+      text: 'A computer once beat me at chess, but it was no match for me at kick boxing.',
     },
     {
       name: 'Craig Bruce',
-      text: 'It’s hardware that makes a machine fast. It’s software that makes a fast machine slow.'
-
+      text: 'It’s hardware that makes a machine fast. It’s software that makes a fast machine slow.',
     },
     {
       name: 'Homer Simpson, in response to the message, “Press any key”',
-      text: 'Where is the ‘any’ key?'
+      text: 'Where is the ‘any’ key?',
     },
   ],
 };
@@ -173,6 +150,6 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/Hizaguru' },
-  { label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/jukka-pekka-lappalainen-0365001a7/' },
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/Hizaguru'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/jukka-pekka-lappalainen-0365001a7/'},
 ];
