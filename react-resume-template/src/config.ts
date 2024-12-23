d8143b0cc67a693e8ce36116919db45c68a5b373
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2024-12-23 11:55:21 +0200

    Update isMobile detection to use window.matchMedia for better accuracy

--- react-resume-template/src/config.ts
+++ react-resume-template/src/config.ts
@@ -1,5 +1,5 @@
 export const isBrowser = typeof window !== 'undefined';
-export const isMobile = true;
+export const isMobile = isBrowser ? window.matchMedia('(pointer: coarse)').matches : false;
 export const canUseDOM: boolean =
   typeof window !== 'undefined' &&
   typeof window.document !== 'undefined' &&
