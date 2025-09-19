bf45db308a4d4473d3d2e8f4938245d6982c845d
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2025-09-19 14:14:03 +0300

    Refactor Modal component for improved readability and consistency

--- react-resume-template/src/components/Modal/Modal.tsx
+++ react-resume-template/src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react';
-import ReactPortal from './ReactPortal';
 import {CSSTransition} from 'react-transition-group';
+import ReactPortal from './ReactPortal';
 
 type Props = {
   isOpen: boolean;
@@ -8,16 +8,14 @@ type Props = {
   handleClose: () => void;
 };
 
+// Modal.tsx
 const Modal: React.FC<Props> = (props: Props) => {
-  const nodeRef = useRef(null);
+  const nodeRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const closeOnEscape = (e: KeyboardEvent) => (e.key === 'Escape' ? props.handleClose() : null);
     document.body.addEventListener('keydown', closeOnEscape);
-
-    return () => {
-      document.body.removeEventListener('keydown', closeOnEscape);
-    };
+    return () => document.body.removeEventListener('keydown', closeOnEscape);
   }, [props.handleClose]);
 
   return (
@@ -28,9 +26,16 @@ const Modal: React.FC<Props> = (props: Props) => {
         unmountOnExit
         classNames="modal"
         nodeRef={nodeRef}>
-        <div className="modal" ref={nodeRef}>
-          <div className="modal-content">
-            <button onClick={props.handleClose} className="close-btn">
+        {/* Modal root should be fixed & top layer */}
+        <div ref={nodeRef} className="modal fixed inset-0 z-[1000]">
+          {/* Backdrop */}
+          <div className="fixed inset-0 bg-black/70" onClick={props.handleClose} />
+          {/* Content */}
+          <div className="modal-content relative z-[1010] mx-auto my-8 max-w-4xl rounded-md bg-white p-4">
+            <button
+              onClick={props.handleClose}
+              className="close-btn absolute right-3 top-3 z-[1020] inline-flex h-9 w-9 items-center justify-center rounded-full bg-black/60 text-white hover:bg-black/80 focus:outline-none focus:ring"
+              aria-label="Close">
               &times;
             </button>
             {props.children}
@@ -40,4 +45,3 @@ const Modal: React.FC<Props> = (props: Props) => {
     </ReactPortal>
   );
 };
-export default Modal;
