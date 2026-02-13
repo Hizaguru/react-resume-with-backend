23538d06d95534df18364d05891be967d646e588
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2026-02-13 11:57:28 +0200

    Add viewport meta tag for responsive design

--- react-resume-template/src/pages/_document.tsx
+++ react-resume-template/src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* google translate breaks react:
           - https://github.com/facebook/react/issues/11538
           - https://bugs.chromium.org/p/chromium/issues/detail?id=872770 */}
